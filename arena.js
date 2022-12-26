f4901fe290c265c8278ec9252296bcd30d141a19
Evandro <evandrosilva8744@gmail.com>
2022-12-25 21:27:36 -0300

    Criação da Arena

--- arena.js
+++ arena.js
@@ -2,22 +2,22 @@ import GameManager from "./game-manager.js";
 
 export default class Arena{
     constructor(){
-        this.columns= GameManager.config.columns;
-        this.lines= GameManager.config.lines;
-        this.width=this.columns * GameManager.config.squareSize;
-        this.height=this.lines  * GameManager.config.squareSize;
+        this._columns= GameManager.config.columns;
+        this._lines= GameManager.config.lines;
+        this._width=this._columns * GameManager.config.squareSize;
+        this._height=this._lines  * GameManager.config.squareSize;
         this.position={
-            top: (GameManager.height  - this._height) / 2,
-            left: (GameManager.width  - this._width) / 2
+            top: (GameManager.config.height  - this._height) / 2,
+            left: (GameManager.config.width  - this._width) / 2
         }
     }
 
     draw(){
     this._drawBorder();
-    //_drawGrid
+    this._drawGrid();
     //_drawSquare
 }
-
+    //Método responsável por desenhar a borda do Game
     _drawBorder() {
         GameManager.context.strokeStyle="#000000";
         GameManager.context.strokeRect(
@@ -27,6 +27,22 @@ export default class Arena{
             this._height
         );
     }
+
+    //Métodos responsável por desenhar a grade atrás do Game
+    _drawGrid(){
+        GameManager.context.strokeStyle="#dedede";
+        GameManager.context.beginPath();
+        for (let i=1; i < this._lines; i++){
+            GameManager.context.moveTo(this.position.left, this.position.top + GameManager.config.squareSize* i);
+            GameManager.context.lineTo(this.position.left + this._width, this.position.top + GameManager.config.squareSize * i);
+        }
+        for (let i=1; i < this._columns; i++){
+            GameManager.context.moveTo(this.position.left + GameManager.config.squareSize * i, this.position.top);
+            GameManager.context.lineTo(this.position.left + GameManager.config.squareSize * i, this.position.top + this._height);
+        }
+        GameManager.context.stroke();
+        }
+        
 
 
 }
