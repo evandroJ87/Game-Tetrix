603e94cbf50a52d04da24346756a77bf06a641a2
Evandro <evandrosilva8744@gmail.com>
2023-01-12 12:43:02 -0300

    att

--- polyomino.js
+++ polyomino.js
@@ -1,16 +1,17 @@
 
-
 export default class Polyomino{
     constructor(squaresCount, color, format){
         this.color= color;
         this._squaresCount= squaresCount;
+        this._initializeSquares(format)
+    }
+        
+    _initializeSquares(format){
         this._squares= [...Array(this._squaresCount)].map(()=>[...Array(this._squaresCount)]);
-
         for (let i=0; i < this._squaresCount; i++) {
             for (let j=0; j<this._squaresCount; j++) {   
                 if (format[i][j]){
-                this._squares[i][j]
-                    this._squares[i][j]= new Square();
+                    this._squares[i][j]= new Square(this.color);
                 }
             }
         }
