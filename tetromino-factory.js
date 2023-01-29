4b5c40341c61be148b6a8828aa1a16375188eecc
Evandro <evandrosilva8744@gmail.com>
2023-01-29 18:32:57 -0300

    att

---
+++ tetromino-factory.js
@@ -0,0 +1,7 @@
+export default class TetrominoFactory {
+    constructor() {
+        this._collection= [
+            
+        ]
+    }
+}
---
+++ tetromino.js
@@ -0,0 +1,7 @@
+import Polyomino from "./polyomino";
+
+export default class Tetromino extends Polyomino{
+constructor(color, format){
+    super (squaresCount, color, format);
+}
+}
